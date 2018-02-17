59498426d3837d4a48c0f6f080d7f61df939c445
Marcos Costa <marcos.lcosta@live.com>
2018-02-16 21:29:45 -0300

    Adicionando validação de telefone, refatorando projeto, renomeando arquivos

--- src/Flunt.Br/Document/Cellphone.cs
+++ src/Flunt.Br/Document/Cellphone.cs
@@ -1,8 +1,10 @@
+using System;
 using Flunt.Br.Document.interfaces;
 using System.Text.RegularExpressions;
 
 namespace Flunt.Br.Document
 {
+    [Obsolete("Use Phone Class")]
     internal class Cellphone : IValidate
     {
         public bool Validate(string value) => new Regex(@"^\(\d{2}\)\d{4,5}-\d{4}$").Match(value).Success;
